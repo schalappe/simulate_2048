78a9e48789b69c1cfa2c4d78ae5f87134c1671db
Kone Abdallah Hakim <71593784+schalappe@users.noreply.github.com>
2022-10-22 12:51:40 +0200

    correction setup

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-# ##: TODO: Mettre à jour le setup.py
+# -*- coding: utf-8 -*-
+"""..."""
 from setuptools import setup
 
 setup(
     name="simulate_2048",
     version="0.0.1",
+    description="Game 2048",
+    author="simulate_2048",
+    packages=["simulate_2048"],
     install_requires=["gym>=0.26.0"],
 )
